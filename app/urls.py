e365bf4627d5e07bc6c07c3314ba589502902b83
coemelo <merlosamuel@protonmail.ch>
2023-10-08 17:54:09 -0300

    Login page

--- app/urls.py
+++ app/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('messenger.urls')),
+    path('auth/', include('users.urls')),
     path('admin/', admin.site.urls),
 ]
